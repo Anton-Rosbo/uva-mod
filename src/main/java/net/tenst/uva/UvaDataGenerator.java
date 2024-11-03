21358bc8d020b47fce0a661640d9d3bc90515b72
Anton Rosbo <antonhelgerosbo@gmail.com>
2024-11-03 17:23:40 +0100

    add base fabric mod structure

---
+++ src/main/java/net/tenst/uva/UvaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tenst.uva;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UvaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
